9cfe8c1bd2837d5bd499477d7701fed56f541504
Joshua Nickings <nickingsnickings2026@u.northwestern.edu>
2025-06-01 21:07:10 -0500

    Completed HW 4

--- HW4/dac3/dac3.c
+++ HW4/dac3/dac3.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
+#include <stdlib.h>
+#include "math.h"
 
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
@@ -23,14 +25,14 @@ static inline void cs_deselect(uint cs_pin) {
     asm volatile("nop \n nop \n nop"); // FIXME
 }
 
-void writeDac();
+void writeDac(int, double);
 
 int main()
 {
     stdio_init_all();
 
-    // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
+    // SPI initialisation.
+    spi_init(SPI_PORT, 1000);
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
     gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
     gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
@@ -42,17 +44,32 @@ int main()
     // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-        writeDac();
+        for (int t = 0; t < 50; t++) { // When making the time period 100, the math doubled the period of each graph. Unsure why! When halved to 50, it got to the desired outcome.
+            sleep_ms(10);
+            double volt_a = (3.3/2) * sin(t * M_PI / 12.5) + (3.3/2);
+            printf("sinvolt %f \n", volt_a);
+            writeDac(0, volt_a);
+
+            double volt_b = abs((25 - t)) * 3.3/25;
+            printf("trivolt %f \n", volt_b);
+            writeDac(1, volt_b);
+        }
     }
 }
 
-void writeDac(){
+void writeDac(int channel, double voltage) {
     uint8_t data[2];
     int len = 2;
-    data[0] = 0b10001000;
-    data[1] = 0b01010101;
+    
+    int conv_volt = (voltage / 3.3) * 1024;
+    printf("convolt %d \n", conv_volt);
+    int conv_volt0 = conv_volt >> 6;
+    printf("convolt0 %d \n", conv_volt0);
+    int conv_volt1 = (conv_volt & 0b0000111111) << 2;
+    printf("convolt1 %d \n\n", conv_volt1);
+
+    data[0] = (channel << 7) | (0b01110000 | conv_volt0); 
+    data[1] = conv_volt1;
     cs_select(PIN_CS);
     spi_write_blocking(SPI_PORT, data, len); // where data is a uint8_t array with length len
     cs_deselect(PIN_CS);
