586cf8fbf2e415e1b8aabb66117c8896ff8a7623
Joshua Nickings <nickingsnickings2026@u.northwestern.edu>
2025-06-03 00:13:24 -0500

    Final Code Finished!

--- HW6/I2C_23008/I2C_23008.c
+++ HW6/I2C_23008/I2C_23008.c
@@ -26,10 +26,11 @@ int main()
 {
     stdio_init_all();
    
-    while (!stdio_usb_connected()) {
+    /*while (!stdio_usb_connected()) {
         sleep_ms(100);
     }
     printf("Start!\n");
+    */
 
     // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400*1000);
@@ -48,6 +49,7 @@ int main()
         // mcp23008_gp7_blink();
         mcp23008_gp0butt_gp7led();
     }
+    
 }
 
 void pico_led_init() {
@@ -58,9 +60,9 @@ void pico_led_init() {
 
 void pico_led_blink() {
     gpio_put(PICO_DEFAULT_LED_PIN, 1);
-    sleep_ms(250);
+    sleep_ms(100);
     gpio_put(PICO_DEFAULT_LED_PIN, 0);
-    printf("LED Blink! \n");
+    // printf("LED Blink! \n");
 }
 
 
@@ -71,7 +73,7 @@ void mpc23008_init() {
     buf[1] = 0b01111111;
 
     i2c_write_blocking(i2c_default, 0b0100000, buf, 2, false);
-    printf("MPC23008 initialization done!\n");
+    // printf("MPC23008 initialization done!\n");
 }
 
 void setPin(unsigned char address, unsigned char reg, unsigned char value) {
@@ -90,7 +92,7 @@ unsigned char readPin(unsigned char address, unsigned char reg) {
 
     i2c_write_blocking(i2c_default, address, &reg, 1, true);
     i2c_read_blocking(i2c_default, address, &buf, 1, false);
-    printf("Pin read!\n");
+    // printf("Pin read!\n");
     return buf;
 }
 
@@ -99,16 +101,17 @@ void mcp23008_gp7_blink() {
     setPin(0b0100000, 10, 0b10000000);
     sleep_ms(250);
     setPin(0b0100000, 10, 0);
-    printf("GP7 shoulda just blinked!\n");
+    // printf("GP7 shoulda just blinked!\n");
 }
 
 void mcp23008_gp0butt_gp7led() {
     //MCP23008 controlled GP0 button determining status of GP7 LED
     
     unsigned char val = readPin(0b0100000, 9) & 1;
+    val = !val;
     val = val << 7;
 
-    printf("%d\n", val);
+    // printf("%d\n", val);
 
     setPin(0b0100000, 10, val);
 }
